35212afcdd921e8ca8e2febca19200f003157d6c
Lenar Gatin <lgatin1711@gmail.com>
2026-03-21 12:51:09 +0500

    feat: add in the command registry all parameterless commands

--- command_registry.py
+++ command_registry.py
@@ -1,7 +1,27 @@
 from scripts.trash_tool import empty_recycle_bin
 from scripts.screen_tool import take_screenshot
+from scripts.turn_off import shutdown
+from scripts.system_toggle_tool import (
+    toggle_wifi,
+    toggle_notifications,
+    toggle_airplane_mode,
+    toggle_bluetooth,
+    toggle_mute
+)
+from scripts.energy_saver_mode import (
+    enable_energy_saver_mode,
+    disable_energy_saver_mode
+)
 
 COMMAND_POOL = {
     "Очистить корзину": empty_recycle_bin,
-    "Скриншот": take_screenshot
+    "Скриншот": take_screenshot,
+    "Выключить компьютер": shutdown,
+    "Переключить интернет": toggle_wifi,
+    "Переключить уведомления": toggle_notifications,
+    "Переключить режим полёта": toggle_airplane_mode,
+    "Переключить блютуз": toggle_bluetooth,
+    "Переключить звук": toggle_mute,
+    "Включить режим энергосбережения": enable_energy_saver_mode,
+    "Выключить режим энергосбережения": disable_energy_saver_mode,
 }
