82f1948a1003af24d661d95283e43060a9394e58
ilyamikhailov16 <ilyamikhailov16@yandex.ru>
2026-03-23 11:23:31 +0500

    fix: busy loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import queue as q
 import threading as th
@@ -100,7 +101,7 @@ if __name__ == "__main__":
     try:
         app.start()
         while True:
-            pass  # keep main thread alive
+            time.sleep(1)
     except KeyboardInterrupt:
         logger.info("Shutting down...")
     finally:
